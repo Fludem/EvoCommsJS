8f321b019d039d1ec497987c373d12c728c9f4e7
Dylan Marriott <dylan@onyxleeds.co.uk>
2025-04-03 05:49:11 +0100

    Feat: Refactor App component to implement routing and theming with Dashboard integration

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,21 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import Dashboard from './pages/dashboard'
+import './index.css'
+import { ThemeProvider } from './components/ui/theme-provider'
+import { Layout } from './components/layout'
 
 function App() {
-
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Navigate to="/dashboard" replace />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+        </Layout>
+      </Router>
+    </ThemeProvider>
   )
 }
 
