8421cad7aeea1ee10ec4fb994c57dcd1a6432624
Dylan Marriott <dylan@onyxleeds.co.uk>
2025-04-07 01:36:32 +0100

    Feat: Update index.ts to export ActivityLogRepository and include customer repository for enhanced module accessibility.

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -1,4 +1,5 @@
 export * from './terminal.repository';
 export * from './employee.repository';
 export * from './clocking.repository';
-export * from './customer.repository'; 
+export * from './customer.repository';
+export * from './activity-log.repository'; 
